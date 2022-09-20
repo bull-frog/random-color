68d76a7bf4abc0bd26d1cc9383926e02d18a9442
bull-frog <bullfrog0603@gmail.com>
2022-09-20 16:45:52 +0900

    6色（明度が同じもの、違うものそれぞれ）

---
+++ colors.js
@@ -0,0 +1,17 @@
+const colors = [
+	'#EA3324',
+	'#0201F5',
+	'#387F23',
+	'#EA33CF',
+	'#EE8733',
+	'#A120EF'
+];
+
+const colorsSameBrightness = [
+	'#EA3324',
+	'#8080F7',
+	'#57BD37',
+	'#DC2FC2',
+	'#D1762C',
+	'#B441F5'
+]
